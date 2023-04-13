f985056b036be0eb26bcfa4c3b8dc14b829518f0
0476a <0476a@naver.com>
2023-04-13 19:34:31 +0900

    2023.04.13(보강)

--- src/main/java/com/web/study/security/PrincipalDetailsService.java
+++ src/main/java/com/web/study/security/PrincipalDetailsService.java
@@ -12,6 +12,8 @@ import com.web.study.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 //사용자정보와 DB의 정보가 맞는지 비교하기위한 것
+//UserDetailsService 불러오면 PrincipalDetailsServiced이걸 실행시켜줌.
+//service 어노테이션을 사용해서 IOC로 넘어가기 때문!
 @Service
 @RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService{
@@ -19,6 +21,7 @@ public class PrincipalDetailsService implements UserDetailsService{
 	private final UserRepository userRepository;
 	
 	//authenticate가 실행되면서 받아온 username 값이 매개변수로 들어가게 됨.
+	//매니저가 해줌.
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User userEntity = userRepository.findUserByUsername(username);
